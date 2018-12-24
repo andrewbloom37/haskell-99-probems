0537db0a1b72fa7ebcdc92e250a9d5c6b4f0ce89
Andrew Bloom <blooma116@gmail.com>
2018-12-23 18:38:41 -0800

    solving flatten problem

--- src/lists1/h07.hs
+++ src/lists1/h07.hs
@@ -1,3 +1,17 @@
 -- (**) Flatten a nested list structure.
 
 -- Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+
+data NestedList a = Elem a | List [NestedList a]
+
+-- Note: due to Haskell's monomorphism restriction, we must use the List type
+--       constructor below in the recursive pattern match, simply stating
+--       `flattenList xs` will cause a compiler error, because it needs to be
+--       explicitly stated that the xs is a List to be a valid member of the
+--       NestedList type in order to be passed in as a parameter to flattenList 
+flattenList :: NestedList a -> [a]
+flattenList list =
+    case list of
+        (Elem x)      -> [x]
+        (List [])     -> []
+        (List (x:xs)) -> flattenList x ++ flattenList (List xs)
