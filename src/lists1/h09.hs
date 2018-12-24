11ca88de9dfe532f67036cc2bb58f510ce8532b6
Andrew Bloom <blooma116@gmail.com>
2018-12-24 00:08:11 -0800

    solving group duplicate elements problem

---
+++ src/lists1/h09.hs
@@ -0,0 +1,14 @@
+-- (**) Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+
+helper :: (Eq a) => [a] -> a -> [a] -> [[a]] -> [[a]]
+helper [] _ currentList outputList = outputList ++ [currentList]
+helper (x:xs) currentVal currentList outputList
+    | x == currentVal = helper xs x (x : currentList) outputList
+    | otherwise       = helper xs x [x] (outputList ++ [currentList])
+
+packList :: (Eq a) => [a] -> [[a]]
+packList list =
+    case list of
+        []     -> []
+        [x]    -> [[x]]
+        (x:xs) -> helper xs x [x] []
